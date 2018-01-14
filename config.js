06dbcbcea4de18288388b1cc4b5022edec4b15c1
rcevol <35377154+rcevol@users.noreply.github.com>
2018-01-14 12:01:01 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,110 @@
+/* Magic Mirror Config Sample
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ *
+ * For more information how you can configurate this file
+ * See https://github.com/MichMich/MagicMirror#configuration
+ *
+ */
+
+var config = {
+	address: "localhost", // Address to listen on, can be:
+	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+	                      // - another specific IPv4/6 to listen on a specific interface
+	                      // - "", "0.0.0.0", "::" to listen on any interface
+	                      // Default, when address config is left out, is "localhost"
+	port: 8080,
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
+	                                                       // or add a specific IPv4 of 192.168.1.5 :
+	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+
+	language: "ro",
+	timeFormat: 24,
+	units: "metric",
+
+	modules: [
+		{
+			module: "alert",
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+		{
+			module: "clock",
+			position: "top_left"
+		},
+		{
+			module: "calendar",
+			header: "CALENDAR SARBATORI",
+			position: "top_left",
+			config: {
+				calendars: [
+					{
+						symbol: "calendar-check-o ",
+						url: "webcal://www.calendarlabs.com/ical-calendar/holidays/romania-holidays-64.ics"
+					}
+				]
+			}
+		},
+		{
+			module: "compliments",
+			position: "lower_third"
+		},
+		{
+			module: "currentweather",
+			position: "top_right",
+			config: {
+				location: "Targoviste",
+				locationID: "665024",  //ID from http://www.openweathermap.org/help/city_list.txt
+				appid: "9a0e3e4ae7351da2114061bd7492886f"
+			}
+		},
+		{
+			module: "weatherforecast",
+			position: "top_right",
+			header: "Vremea probabila",
+			config: {
+				location: "Targoviste",
+				locationID: "665024",  //ID from http://www.openweathermap.org/help/city_list.txt
+				appid: "9a0e3e4ae7351da2114061bd7492886f"
+			}
+		},
+		{   //Yours starts here at the {
+                        module: ‘email’,
+                        position: ‘bottom_left’,
+                        header: ‘Email’,
+                        config:{
+                        user: ‘cosmin.florica@gmail.com’,
+                        password: ‘EvOl!979’,
+                        host: ‘imap.gmail.com’,
+                        port: 993,
+                        tls: true,
+                        authTimeout: 10000,
+                        numberOfEmails: 5,
+                        fade: true
+                }   // No comma here!
+                },  // Yours ends here
+		{
+			module: "newsfeed",
+			position: "bottom_bar",
+			config: {
+				feeds: [
+					{
+						title: "Stiri",
+						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true
+			}
+		},
+	]
+
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {module.exports = config;}
